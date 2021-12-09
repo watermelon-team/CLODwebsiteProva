7a196bab934d942f36066a80bde69fff2c89b7f7
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2021-12-09 08:42:46 +0100

    start

---
+++ sketch.js
@@ -0,0 +1,117 @@
+function accordationTech(obj) {
+  console.log(obj.id);
+  var square = document.getElementById('squareTech');
+  square.classList.remove('modularSystemBg');
+  square.classList.remove('trackerBg');
+  square.classList.remove('guideBg');
+
+  square.classList.add(obj.id);
+}
+
+
+// scroll tech
+
+
+document.addEventListener("scroll", function(e) {
+
+  // if (window.innerWidth >= window.innerHeight) {
+
+    function offset(el) {
+      var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {
+        top: rect.top + scrollTop
+      }
+    }
+
+    var div = document.getElementById('rowTech');
+    var tech01 = $('#tech01');
+    var tech02 = $('#tech02');
+    var tech03 = $('#tech03');
+    var tech04 = $('#tech04');
+    var tech05 = $('#tech05');
+    var tech06 = $('#tech06');
+    var tech07 = $('#tech07');
+
+    var techTotal = $('.techContain');
+
+
+
+    var divOffset = offset(div);
+
+    if (window.scrollY >= (divOffset.top - 100) && window.scrollY <= (divOffset.top + 500)) {
+      console.log('uguale!');
+
+      techTotal.each(function(i) {
+        techTotal.eq(i).addClass('techFixed');
+      });
+
+      tech02.addClass('techInactive');
+      tech02.removeClass('techActive');
+
+    } else if (window.scrollY >= (divOffset.top + 500) && window.scrollY <= (divOffset.top + 1000)) {
+      tech02.addClass('techActive');
+      tech02.removeClass('techInactive');
+
+      tech03.addClass('techInactive');
+      tech03.removeClass('techActive');
+
+    } else if (window.scrollY >= (divOffset.top + 1000) && window.scrollY <= (divOffset.top + 1500)) {
+      tech03.addClass('techActive');
+      tech03.removeClass('techInactive');
+
+      tech04.addClass('techInactive');
+      tech04.removeClass('techActive');
+
+    } else if (window.scrollY >= (divOffset.top + 1500) && window.scrollY <= (divOffset.top + 2000)) {
+      tech04.addClass('techActive');
+      tech04.removeClass('techInactive');
+
+      tech05.addClass('techInactive');
+      tech05.removeClass('techActive');
+
+    } else if (window.scrollY >= (divOffset.top + 2000) && window.scrollY <= (divOffset.top + 2500)) {
+      tech05.addClass('techActive');
+      tech05.removeClass('techInactive');
+
+      tech06.addClass('techInactive');
+      tech06.removeClass('techActive');
+
+    } else if (window.scrollY >= (divOffset.top + 2500) && window.scrollY <= (divOffset.top + 3000)) {
+      tech06.addClass('techActive');
+      tech06.removeClass('techInactive');
+
+      tech07.addClass('techInactive');
+      tech07.removeClass('techActive');
+
+    } else if (window.scrollY >= (divOffset.top + 3000) && window.scrollY <= (divOffset.top + 3500)) {
+      techTotal.each(function(i) {
+        techTotal.eq(i).addClass('techFixed');
+      });
+
+      tech07.addClass('techActive');
+      tech07.removeClass('techInactive');
+
+    } else if(window.scrollY >= (divOffset.top + 3500)){
+      techTotal.each(function(i) {
+        techTotal.eq(i).removeClass('techFixed');
+        if(i>=1){
+          techTotal.eq(i).addClass('techInactive');
+          techTotal.eq(i).removeClass('techActive');
+        }
+      });
+
+
+
+
+    }  else if (window.scrollY <= (divOffset.top - 100)) {
+      techTotal.each(function(i) {
+        techTotal.eq(i).removeClass('techFixed');
+      });
+    }
+
+
+  // }
+
+})
